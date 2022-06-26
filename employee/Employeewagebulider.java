a40fa88b89f5ee97d9704dc5e642d839279b877d
prashant6311 <prashantppatil761@gmail.com>
2022-06-26 11:16:37 +0530

    ADDED:employee daily wage

--- employee/Employeewagebulider.java
+++ employee/Employeewagebulider.java
@@ -3,12 +3,18 @@ package com.bridgrlabz.employee;
 public class Employeewagebulider {
 
 	public static void main(String[] args) {
+		// use case 1:check employee attendence
 		// CONSTANTS
 		int ISPRESENT = 1;
+		int wagePerHour = 20;
+		int workingHrs = 8;
 		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if (empCheck == ISPRESENT)
+		if (empCheck == ISPRESENT) {
+			// use case 2:employee daily wage
+			int empwage = wagePerHour * workingHrs;
 			System.out.println("Employee is present");
-		else
+			System.out.println("Employee wage = " + empwage);
+		} else
 			System.out.println("Employee is abscent");
 	}
 }
