4d2c9db2afa347b7beb6a39466bdca9768420f14
prashant6311 <prashantppatil761@gmail.com>
2022-06-27 14:20:21 +0530

    ADDED: Day And Hour

--- employee/Employeewagebulider.java
+++ employee/Employeewagebulider.java
@@ -6,14 +6,16 @@ public class Employeewagebulider {
 	public static final int isFullTime = 2;
 	public static final int empRatePerHour = 20;
 	public static final int noOfWorkingDay = 20;
+	public static final int maxHrsInMonth = 100;
 
 	public static void main(String[] args) {
 		// VARIABLE
 		int empHrs = 0;
-		int empWage = 0;
-		int totalEmpWage = 0;
+		int totalEmpHrs = 0;
+		int totalWorkingDay = 0;
 		// computation
-		for (int day = 0; day < noOfWorkingDay; day++) {
+		while (totalEmpHrs <= maxHrsInMonth && totalWorkingDay < noOfWorkingDay) {
+			totalWorkingDay++;
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (empCheck) {
 			case isPartTime:
@@ -25,12 +27,12 @@ public class Employeewagebulider {
 			default:
 				empHrs = 0;
 			}
-				empWage = empHrs * empRatePerHour;
-				totalEmpWage += empWage;
-				System.out.println("Emp Wage: " + empWage);
-			}
-			System.out.println("total Emp Wage: = " + totalEmpWage);
+			totalEmpHrs += empHrs;
 
-		
+			System.out.println("Day#: " + totalWorkingDay + " EmpHrs: " + empHrs);
+		}
+		int totalEmpWage = totalEmpHrs * empRatePerHour;
+		System.out.println("total Emp Wage: = " + totalEmpWage);
+
 	}
 }
